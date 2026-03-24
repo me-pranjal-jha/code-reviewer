6a6b0a2c89be029227b548caadd35c7012043437
Cranial Belgrade <prnjljh@gmail.com>
2026-03-25 02:52:49 +0530

    Made slight changes in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const app = require('./src/app.js');
+const PORT = 3000 || process.env.PORT
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL }))
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
 // console.log("API KEY:", process.env.GOOGLE_GEMINI_KEY);
